d246fc1d83573315d05c32ad8b8e39b1f934c064
cristianocorrea3 <66446324+cristianocorrea3@users.noreply.github.com>
2022-09-01 07:41:06 -0300

    Criação do menu de acesso e tela de listagem de categorias. Instalação do pacote axios.
    Criação do menu de acesso e tela de listagem de categorias. Instalação do pacote axios.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+const axios = require('axios').default;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -11,10 +12,26 @@ app.use(express.static('public'));
 //CONFIGURA O EJS COMO VIEW ENGINE (REDENRIZA AS PÁGINAS DE FRONT-END)
 app.set('view engine', 'ejs');
 
-//ROTA DE TESTE DE RENDERIZAÇÃO
-app.get('/', (req, res)=>{
-    res.render('index');
-});
+//ROTA DE LISTAGEM DE CATEGORIAS
+app.get('/listagemCategorias', (req, res)=>{
+    
+    const urlListagemCategoria = 'http://localhost:3000/listarCategoria';
+
+    /*
+    CHAMADA PELO AXIOS:
+    1 - URL DA ROTA (urlListagemCategoria)
+    2 - CALLBACK DA RESPOSTA DA CHAMADA
+    */
+    axios.get(urlListagemCategoria)
+        .then(
+            (response)=>{
+                // console.log(response.data);
+                // res.send(response.data);
+                let categorias = response.data;
+                res.render('categoria/listagemCategoria', {categorias});
+
+        }); 
+    });
 
 app.listen(3001, ()=>{
     console.log('SERVIDOR RODANDO EM: http://localhost:3001');
